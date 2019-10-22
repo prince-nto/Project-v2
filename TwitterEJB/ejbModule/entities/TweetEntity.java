e4cb7d9c5b6dfdb9d6bade7f95939222bccbfc9c
Prince Ntombela <pntombela@outlook.com>
2019-10-22 07:52:05 +0200

    Init
    Init

---
+++ TwitterEJB/ejbModule/entities/TweetEntity.java
@@ -0,0 +1,40 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+@Entity(name="twitter_tbl")
+public class TweetEntity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long tweetId;
+	private String body;
+	private DateTimeAtCompleted dateCreated;
+	
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	public void setDateCreated(DateTimeAtCompleted dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+	
+	
+	public long getTweetId() {
+		return tweetId;
+	}
+	
+	public String getBody() {
+		return body;
+	}
+	
+	public DateTimeAtCompleted getDateCreated() {
+		return dateCreated;
+	}
+	
+	
+}
